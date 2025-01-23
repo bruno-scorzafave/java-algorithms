03250fd9e4a94d7b49930cc132375879122d895d
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2025-01-23 18:06:20 -0300

    Add Honer's Rule

---
+++ HonersRule/HonersRule.java
@@ -0,0 +1,11 @@
+package HonersRule;
+
+public class HonersRule {
+    public static int honersRule(int[] arr, int length, int x) {
+        int p  = 0;
+        for (int i = length - 1; i >= 0; i--) {
+            p = arr[i] + p * x;
+        }
+        return p;
+    }
+}
