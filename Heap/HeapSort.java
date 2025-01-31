7e2f517d6f4b71e66cb4157720bd6a7779cf56ce
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2025-01-31 18:48:27 -0300

    Crate heapsort

---
+++ Heap/HeapSort.java
@@ -0,0 +1,17 @@
+package Heap;
+
+public class HeapSort {
+    public void heapSort(Heap heap) {
+        for (int i = heap.size - 1; i >= 1; i--) {
+            swap(heap.heap, 0, i);
+            heap.size--;
+            heap.heapify(0);
+        }
+    }
+
+    public void swap(int[] arr, int firstNode, int secondNode) {
+        int tmp = arr[firstNode];
+        arr[firstNode] = arr[secondNode];
+        arr[secondNode] = tmp;
+    }
+}
