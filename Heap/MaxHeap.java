10e2431c69bfc3b953b9cd971d65e5ee2db95aa7
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2025-01-31 18:47:24 -0300

    Adapt MaxHeap

--- Heap/MaxHeap.java
+++ Heap/MaxHeap.java
@@ -1,60 +1,34 @@
 package Heap;
 
-public class MaxHeap {
-    private int size;
-    private int[] maxHeap;
-
+public class MaxHeap extends Heap {
     public MaxHeap(int maxSize, int[] arr) {
         this.size = maxSize;
-        this.maxHeap = new int[this.size];
-        this.maxHeap = arr;
+        this.heap = arr;
     }
 
-    public void maxHeapify(int index) {
+    @Override
+    public void heapify(int index) {
         int left = leftChild(index);
         int right = rightChild(index);
         int largest = index;
 
-        if (left < size && maxHeap[left] > maxHeap[largest]) {
+        if (left < size && heap[left] > heap[largest]) {
             largest = left;
         }
 
-        if (right < size && maxHeap[right] > maxHeap[largest]) {
+        if (right < size && heap[right] > heap[largest]) {
             largest = right;
         }
 
         if (largest != index) {
             swap(index, largest);
-            maxHeapify(largest);
+            heapify(largest);
         }
-    }
-
-    public void swap(int firstNode, int secondNode) {
-        int tmp = maxHeap[firstNode];
-        maxHeap[firstNode] = maxHeap[secondNode];
-        maxHeap[secondNode] = tmp;
     }
 
     public void buildMaxHeap() {
         for (int i = size / 2 - 1; i >= 0; i--) {
-            maxHeapify(i);
+            heapify(i);
         }
-    }
-
-    @Override
-    public String toString() {
-        String str = "";
-        for (int i = 0; i < size; i++) {
-            str += maxHeap[i] +" ";
-        }
-        return str;
-    }
-
-    public int rightChild(int i) {
-        return (2*i) + 2;
-    }
-
-    public int leftChild(int i) {
-        return (2*i) + 1;
     }
 }
