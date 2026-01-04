d23bd000d8e20680f740fb41ee7914f69a56d9c9
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2026-01-04 17:30:56 -0300

    Solve exercise 4.6-d

--- exercises/problem4_6.java
+++ exercises/problem4_6.java
@@ -1,47 +1,83 @@
 package exercises;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class problem4_6 {
     public static int findGoodChip(int[] chips) {
-        if (chips.length == 1) {
-            return chips[0];
+        List<Integer> allIndexes = new ArrayList<>();
+        for (int i = 0; i < chips.length; i++) {
+            allIndexes.add(i);
+        }
+        
+        return findGoodChipRecursive(chips, allIndexes);
+    }
+
+    public static int findGoodChipRecursive(int[] chips, List<Integer> indexes) {
+        if (indexes.size() == 1) {
+            return indexes.getFirst();
         }
 
-        int[] moreThanHalfGoodChips = new int[chips.length/2 + 1];
-        int index = 0;
+        List<Integer> survivors = new ArrayList<>();
 
-        for (int i = 0; i < chips.length - 1; i += 2) {
-            int[] pairwiseTest = pairwiseTest(chips[i], chips[i + 1]);
+        for (int i = 0; i < indexes.size() - 1; i+=2) {
+            int[] pairwiseTest = pairwiseTest(chips[indexes.get(i)], chips[indexes.get(i + 1)]);
 
             if (pairwiseTest[0] == 0 && pairwiseTest[1] == 0) {
-                moreThanHalfGoodChips[index] = chips[i];
-                index ++;
+                survivors.add(indexes.get(i));
             }
         }
 
-        if (chips.length % 2 == 1) {
-            moreThanHalfGoodChips[index] = chips[chips.length - 1];
+        if (indexes.size() % 2 == 1) {
+            int lastIndex = indexes.get(indexes.size() - 1);
+            survivors.add(lastIndex);
         }
 
-        int goodChip = findGoodChip(moreThanHalfGoodChips);
+        if (survivors.isEmpty()) {
+            return indexes.getFirst();
+        }
 
-        return goodChip;
+        return findGoodChipRecursive(chips, survivors);
+    }
+
+    public static int[] findAllGoodChips(int[] chips) {
+        int goodChipIndex = findGoodChip(chips);
+
+        List<Integer> listOfAllGoodChips = new ArrayList<>();
+        listOfAllGoodChips.add(goodChipIndex);
+
+        for (int i = 0; i < chips.length; i++) {
+            if (i == goodChipIndex) {
+                continue;
+            }
+            int[] pairwiseTest = pairwiseTest(chips[goodChipIndex], chips[i]);
+
+            if (pairwiseTest[0] == 0) {
+                listOfAllGoodChips.add(i);
+            }
+        }
+
+        int[] arrayOfAllGoodChips = listOfAllGoodChips.stream().mapToInt(i -> i).toArray();
+
+        return arrayOfAllGoodChips;
     }
 
     public static int[] pairwiseTest(int chip1, int chip2){
         int[] result = new int[2];
         
-        if (chip1 == 0) {
-            result[0] = chip2;
-        } else {
-            result[0] = (int) Math.round(Math.random());
-        }
-
-        if (chip2 == 0) {
-            result[1] = chip1;
-        } else {
-            result[1] = (int) Math.round(Math.random());
-        }
+        result[0] = (chip1 == 0) ? chip2 : (int) Math.round(Math.random());
+        result[1] = (chip2 == 0) ? chip1 : (int) Math.round(Math.random());
 
         return result;
+    }
+
+    public static void main(String[] args) {
+        int[] chips = {1, 0, 0, 0, 1, 1, 0};
+
+        int[] goodChipsIndex = findAllGoodChips(chips);
+
+        for (int chipIndex : goodChipsIndex) {
+            System.out.println("Good chip index: " + chipIndex);
+        }
     }
 }
